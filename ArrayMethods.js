e58dee97d1ce52d5adb3256d48ad0de2c71902a7
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-08-06 00:05:58 +0530

    first commit

---
+++ ArrayMethods.js
@@ -0,0 +1,20 @@
+// let arr = [1, 3, 6, 7, 4, 8, 45]
+// console.log(arr.length)
+// delete [0]
+// console.log(arr.length)
+
+// //concat method.
+
+// arr1 = [51, 33, 63, 74, 45, 87, 46]
+// arr2 = [31, 53, 56, 37, 44, 58, 65]
+
+// let Arr = arr1.concat(arr2)
+// console.log(Arr)
+
+// Sort method.
+let compare = (a, b)=>{
+    return a - b                   // sorting array using function in ascending order.
+}
+let num = [2, 65, 3, 6, 5, 1, 45, 67, 4]
+num.sort(compare)
+console.log(num);
