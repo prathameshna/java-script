bdf26fce6a9105d988b514e79064d85e6e5edd20
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-08-20 22:32:02 +0530

    commit

--- htmlPage/PractiseSet6.1.js
+++ htmlPage/PractiseSet6.1.js
@@ -1,14 +1,16 @@
 // Write a program using prompt function to take input of age as a value from the user and use alert  to tell him if he can drive?
-let age = prompt("what is your age :")
+
+age = prompt("What is your age")
 age = Number.parseInt(age)
 
-const CanDrive  = (age)=>{
-  return age>=18?true:false
-} 
+const CanDrive = (age) => {
+    return age > 18 ? true : false
+}
 
 if (CanDrive(age)) {
-  alert("you can drive")
-
-} else {
-  alert("you can not drive")
+    alert("Ypu can drive")
 }
+else {
+    alert("you can not drive")
+}
+
--- htmlPage/script.js
+++ htmlPage/script.js
@@ -1,1 +1,14 @@
-console.log(console);
+
+let age = prompt("what is your age :")
+age = Number.parseInt(age)
+
+const CanDrive = (age) => {
+    return age > 18 ? true : false
+}
+
+if (CanDrive(age)) {
+    alert("you can drive")
+
+} else {
+    alert("you can not drive")
+}
