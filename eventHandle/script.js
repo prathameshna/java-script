56d7f26848fcb5f3e37d5797897ed3377c06094f
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-09-01 10:51:25 +0530

    fisrt commit

---
+++ eventHandle/script.js
@@ -0,0 +1,18 @@
+// Event Handlers
+
+function myfunction() {
+    alert("thank you click me")
+}
+
+const mouseover = () =>{
+    console.log('hey you click on this bro');
+    
+}
+
+// Event Listner
+
+const evnt = document.getElementById('evnt');
+evnt.addEventListener('click', () => {
+    console.log('click on avent');
+    
+})
