7f95f64fb89fb6eb2f957b7c58a824b23cfc9351
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-10-01 18:15:14 +0530

    first commit

---
+++ Inner&OuterHtml/script.js
@@ -0,0 +1,12 @@
+let x = document.getElementsByTagName('span')[0]
+console.log(x);
+
+let y = document.getElementsByTagName('span')[0]
+console.dir(y);
+
+// INNER HTML
+  first.innerHtml = "this use for set html"
+
+
+  // OUTER HTML 
+  first.outerHtml = "<div>hey</div>"
