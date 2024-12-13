9361f5c7ba2af2f711c35863f556cba6c628b481
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-12-13 06:15:06 +0530

    commit

---
+++ SetTimeOut/script.js
@@ -0,0 +1,19 @@
+// 
+// setTimeout(function(){
+//     alert("I am inside a settimeout")
+// },2000)
+
+
+// setInterval(function(){
+//     alert("after every 10s")
+// },3000)
+
+// Timeout using if and prompt statement
+// 
+
+// SetimeOut using function and passing argument
+const sum =(a,b,c)=>{
+     console.log("this is your ans" +(a+b+c))
+     a+b+c
+}
+setTimeout(sum,5000,2,2,2)
--- 📬rada.js
+++ 📬rada.js
@@ -1,3 +1,5 @@
-
-
-
+marks ={harry:98,rohan:70,akash:7}
+for(int a = marks; a++){
+    console.log(a);
+    
+}
